a1005f6cfd313ed42fd436102ca7a7d5eccb1255
Amit <amitsuyal09@gmail.com>
2026-05-11 12:16:25 +0530

    app.js

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.use(function(res,req,next){
-    console.log("This is a middleware!");
-    next();
-})
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.get('/', (req, res) => {
-  res.send('Hello World changed')
-})
-app.get("/profile", (req, res, next) =>{
-    return next(new Error("Something went wrong!"))
-})
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
+app.get("/", (req, res) => {
+  res.send("Docker running");
+});
 
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
